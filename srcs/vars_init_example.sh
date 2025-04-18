f1e1472913f9cf119d25cb4e1e23ebb035b248d6
alexkasiot <alex_kasiot@hotmail.com>
2025-04-18 17:05:48 +0200

    yet another improvement to the last 2 commits

--- srcs/vars_init_example.sh
+++ srcs/vars_init_example.sh
@@ -1,28 +1,30 @@
 #!/bin/bash
 
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd)"
+
 # Set up secrets files
-echo "[your value]" | sudo tee .secrets/wp_mysql_password.txt
+echo "[your value]" | sudo tee ${SCRIPT_DIR}/.secrets/wp_mysql_password.txt
 
-echo "[your_value]" | sudo tee .secrets/wp_mysql_root_password.txt
+echo "[your_value]" | sudo tee ${SCRIPT_DIR}/.secrets/wp_mysql_root_password.txt
 
-echo "[your_value]" | sudo tee .secrets/wp_admin_password.txt
+echo "[your_value]" | sudo tee ${SCRIPT_DIR}/.secrets/wp_admin_password.txt
 
 
 # Set up .env file
-echo 'DOMAIN_NAME=' >> .env
-echo >> .env
-echo '# Database' >> .env
-echo 'MYSQL_NAME=' >> .env
-echo 'MYSQL_USER=' >> .env
-echo >> .env
-echo '# Wordpress' >> .env
-echo 'WP_DB_NAME=${MYSQL_NAME}' >> .env
-echo 'WP_DB_USER=${MYSQL_USER}' >> .env
-echo 'WP_DB_HOST=mariadb:3306' >> .env
-echo 'WP_ADMIN_USER=' >> .env
-echo '# WP_ADMIN_EMAIL=' >> .env
+echo 'DOMAIN_NAME=' >> ${SCRIPT_DIR}/.env
+echo >> ${SCRIPT_DIR}/.env
+echo '# Database' >> ${SCRIPT_DIR}/.env
+echo 'MYSQL_NAME=' >> ${SCRIPT_DIR}/.env
+echo 'MYSQL_USER=' >> ${SCRIPT_DIR}/.env
+echo >> ${SCRIPT_DIR}/.env
+echo '# Wordpress' >> ${SCRIPT_DIR}/.env
+echo 'WP_DB_NAME=${MYSQL_NAME}' >> ${SCRIPT_DIR}/.env
+echo 'WP_DB_USER=${MYSQL_USER}' >> ${SCRIPT_DIR}/.env
+echo 'WP_DB_HOST=mariadb:3306' >> ${SCRIPT_DIR}/.env
+echo 'WP_ADMIN_USER=' >> ${SCRIPT_DIR}/.env
+echo '# WP_ADMIN_EMAIL=' >> ${SCRIPT_DIR}/.env
 
 sed -i -e 's/DOMAIN_NAME=/DOMAIN_NAME=[your value]/' \
        -e 's/MYSQL_NAME=/MYSQL_NAME=[your value]/' \
 	   -e 's/MYSQL_USER=/MYSQL_USER=[your value]/' \
-	   -e 's/WP_ADMIN_USER=/WP_ADMIN_USER=[your value]/' .env
+	   -e 's/WP_ADMIN_USER=/WP_ADMIN_USER=[your value]/' ${SCRIPT_DIR}/.env
