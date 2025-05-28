5c9990d2c9071ea699f92c37b5c1dc7f8ea5a555
alexkasiot <alex_kasiot@hotmail.com>
2025-05-28 15:15:05 +0200

    implemented a healthcheck for mariadb

--- srcs/requirements/wordpress/tools/wordpress_init.sh
+++ srcs/requirements/wordpress/tools/wordpress_init.sh
@@ -4,11 +4,6 @@ WP_DB_PASSWORD=$(cat /run/secrets/wp_mysql_password)
 WP_ADMIN_PASSWORD=$(cat /run/secrets/wp_admin_password)
 WP_BASIC_PASSWORD=$(cat /run/secrets/wp_basic_password)
 
-# Allow mariadb to start, will implement a better fix, doesn't really matter in this project
-until mysqladmin -h${WP_DB_HOST} -u${WP_DB_USER} -p${WP_DB_PASSWORD} ping; do
-	sleep 2
-done
-
 FILE=/var/www/html/wp-config.php
 if [ ! -f "$FILE" ];  then
 	wp config create --allow-root \
